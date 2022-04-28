e261dbf2f2ec6a378e18719c44a3524cce280ae0
kpc-kwansuda <nearlymorning@gmail.com>
2022-04-29 03:00:04 +0700

    mobile menu in progress

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
 
 // Desktop Navigation
 
-$('.hasSub').click((e)=>{
-    if($(e.currentTarget).hasClass('active')){
-        subMenuClose(e);
-    } else {
-        subMenuOpen(e);
-    }
-});
+if($(window).width()<576){
+    //Mobile
+    $('.mobileMenu').click(()=>{
+        if($('.mobileMenu').hasClass('active')){
+            mobileMenuClose('.mobileMenu','#mainMenu')
+        } else {
+            mobileMenuOpen('.mobileMenu','#mainMenu')
+        }
+    })
+    $('.hasSub').click((e)=>{
+        if($(e.currentTarget).hasClass('active')){
+            subMenuClose(e)
+        } else {
+            subMenuOpen(e)
+        }
+    });
+    $(window).scroll(()=>{
+        mobileMenuClose('.mobileMenu','#mainMenu')
+    })
+    $(window).click(()=>{
+        mobileMenuClose('.mobileMenu','#mainMenu')
+    })
+} else{
+    //Desktop
+    $('.hasSub').hover((e)=>{
+        subMenuOpen(e)
+    }, (e)=>{
+        subMenuClose(e)
+    });
+}
+
+
+
+let mobileMenuOpen =(btn,menu)=>{
+    $(btn).addClass('active')
+    $(menu).css('width','65%')
+    $('.pageOverlay').css('height','100%')
+
+}
+let mobileMenuClose =(btn,menu)=>{
+    $(btn).removeClass('active')
+    $(menu).css('width','0')
+    $('.pageOverlay').css('height','0')
+}
 
 let subMenuOpen =(e)=> {
     $(e.currentTarget).addClass('active')
@@ -15,13 +52,13 @@ let subMenuOpen =(e)=> {
         'height':'auto',
         'padding-top':'15px',
         'padding-bottom':'15px'
-    });
+    })
 }
 let subMenuClose =(e)=> {
-    $(e.currentTarget).removeClass('active');
+    $(e.currentTarget).removeClass('active')
     $(e.currentTarget).find('.submenu').css({
         'height':'0',
         'padding-top':'0',
         'padding-bottom':'0'
-    });
+    })
 }
